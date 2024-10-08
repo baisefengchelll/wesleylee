cc7668466fe1fe0c4d5abd16bf8bc05d39665758
Wesley <leewesley@WesleydeMacBook-Pro.local>
2024-10-08 15:34:53 +0800

    linkqueue study

--- datastruct/queue/linkqueue.h
+++ datastruct/queue/linkqueue.h
@@ -16,13 +16,49 @@ typedef struct
 void initLinkQueue(LinkQueue &lq)
 {
     lq.front = lq.rear = new Qnode;
-    if(!lq.front) exit(false);
+    if (!lq.front)
+        exit(false);
     lq.front->next = nullptr;
 }
-
+// idea：从队列头节点开始，依次释放所有节点
 void destroyQLinkQueue(LinkQueue &lq)
 {
-    while(lq.front){
-        Qnode* p = lq.front->next;
-    }
+    while (lq.front)
+    {
+        Qnode *p = lq.front->next;
+        delete lq.front;
+        lq.front = p;
+    } // lq.rear = lq.front-next; delete lq.front; lq.front = lq.rear; 直接使用rear指针
+}
+// 链列队操作——将元素e入队
+void EnLinkQueue(LinkQueue &lq, QElemType e)
+{
+    Qnode *p = new Qnode;
+    if (!p)
+        exit(false);
+    p->data = e;
+    p->next = nullptr;
+    lq.rear->next = p;
+    lq.rear = p;
+}
+
+// 链队列操作——将元素e出对
+void DeLinkQueue(LinkQueue &lq, QElemType &e)
+{
+    if (lq.front == lq.rear)
+        exit(false);
+    Qnode *p = lq.front->next;
+    e = p->data;
+    lq.front->next = p->next;
+    if (lq.rear == p)
+        lq.rear = lq.front;
+    delete p;
+}
+
+// 链队列操作——求链队列的对头元素
+void Gethead(LinkQueue &lq, QElemType &e)
+{
+    if (lq.front == lq.rear)
+        exit(false);
+    e = lq.front->next->data;
 }
