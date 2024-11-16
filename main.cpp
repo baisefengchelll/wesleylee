62548902505e97b52e5a573b186ba824bcf5c242
baisefengchelll <baisefengche46@gmail.com>
2024-11-16 12:26:34 +0800

    Update

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
+#include <fstream>
+
 using namespace std;
 int main()
 {
-  int a = 10;
-  int b = 20;
-  cout<<"a:"<<a<<endl;
-  cout << "b:"<<b<<endl;
+  fstream file;
+  // open the file
+  file.open("test.txt", ios::out);
+  if(!file.is_open()){
+    cout << "The file didn't be opened rightly!"<<endl;
+    return -1;
+  }
+  
+  // operate the file
+  file << "This is the write file" << endl;
+  file << "Hello world!!!" << endl;
+  file << "Operation finished!" << endl;
 
+  // close the file
+  file.close();
   return 0;
 }
