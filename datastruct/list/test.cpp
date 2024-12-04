7c9671aa3276296654ba29de6d676512b34f7ec1
baisefengchelll <baisefengche46@gmail.com>
2024-12-04 23:59:23 +0800

    update

--- datastruct/list/test.cpp
+++
@@ -1,9 +0,0 @@
-#include "studentlist.h"
-
-int main()
-{
-    Student s1 = {"foo qiuling", "600000000", 23};
-    s1.show();
-
-    return 0;
-}
--- datastruct/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <string.h>
-int main()
-{
-    char a1[10] = "abac";
-    std::cout << strlen(a1) << std::endl;
-}
